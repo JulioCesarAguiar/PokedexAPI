d26ab12a3b03813c0a43bcdb06784f26320a209e
julio <juliocafn2004@gmail.com>
2024-08-25 16:15:36 -0300

    first commit

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -70,3 +70,15 @@ btnNext.addEventListener('click', () => {
 });
 
 renderPokemon(searchPokemon);
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/src/service-worker.js')
+      .then(registration => {
+        console.log('Service Worker registered with scope:', registration.scope);
+      })
+      .catch(error => {
+        console.log('Service Worker registration failed:', error);
+      });
+  });
+}
